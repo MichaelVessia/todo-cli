c4f3ae925d9599e7eed31d6057453ef73a4e96b6
Michael Vessia <michael@vessia.net>
2025-07-06 22:25:40 -0400

    Use errors in the repository interface

--- src/domain/todo/TodoRepository.ts
+++ src/domain/todo/TodoRepository.ts
@@ -1,15 +1,16 @@
 import { Context, Effect } from "effect"
 import { Todo } from "./Todo.js"
 import { TodoId } from "./TodoId.js"
+import { TodoNotFoundError, TodoRepositoryError, TodoAlreadyExistsError } from "./TodoErrors.js"
 
 export interface TodoRepository {
-  readonly findById: (id: TodoId) => Effect.Effect<Todo | null, never, never>
-  readonly findAll: () => Effect.Effect<readonly Todo[], never, never>
-  readonly save: (todo: Todo) => Effect.Effect<Todo, never, never>
-  readonly deleteById: (id: TodoId) => Effect.Effect<boolean, never, never>
-  readonly findByStatus: (status: Todo["status"]) => Effect.Effect<readonly Todo[], never, never>
-  readonly findByPriority: (priority: Todo["priority"]) => Effect.Effect<readonly Todo[], never, never>
-  readonly count: () => Effect.Effect<number, never, never>
+  readonly findById: (id: TodoId) => Effect.Effect<Todo, TodoNotFoundError | TodoRepositoryError, never>
+  readonly findAll: () => Effect.Effect<readonly Todo[], TodoRepositoryError, never>
+  readonly save: (todo: Todo) => Effect.Effect<Todo, TodoAlreadyExistsError | TodoRepositoryError, never>
+  readonly deleteById: (id: TodoId) => Effect.Effect<void, TodoNotFoundError | TodoRepositoryError, never>
+  readonly findByStatus: (status: Todo["status"]) => Effect.Effect<readonly Todo[], TodoRepositoryError, never>
+  readonly findByPriority: (priority: Todo["priority"]) => Effect.Effect<readonly Todo[], TodoRepositoryError, never>
+  readonly count: () => Effect.Effect<number, TodoRepositoryError, never>
 }
 
 export const TodoRepository = Context.GenericTag<TodoRepository>("TodoRepository")
