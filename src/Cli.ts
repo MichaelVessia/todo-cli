055b6679ee07e8eac287f51d896999e2dc614f73
Michael Vessia <michael@vessia.net>
2025-07-06 22:01:19 -0400

    Scaffold other commands

--- src/Cli.ts
+++ src/Cli.ts
@@ -5,8 +5,24 @@ const addCommand = Command.make("add", {}, () =>
   Console.log("Adding todo...")
 )
 
+const listCommand = Command.make("list", {}, () =>
+  Console.log("Listing todos...")
+)
+
+const updateCommand = Command.make("update", {}, () =>
+  Console.log("updating todo...")
+)
+
+const removeCommand = Command.make("remove", {}, () =>
+  Console.log("removing todo...")
+)
+
+const completeCommand = Command.make("complete", {}, () =>
+  Console.log("complete todo...")
+)
+
 const command = Command.make("todo").pipe(
-  Command.withSubcommands([addCommand])
+  Command.withSubcommands([addCommand, listCommand, updateCommand, removeCommand, completeCommand])
 )
 
 export const run = Command.run(command, {
